e5debb20d22a22c71e1f05c79f227680fd847c84
Moises <loveloungeoficial@gmail.com>
2020-04-01 19:01:25 -0300

    View detalhes de evento

--- src/main/java/com/i9/eventoapp/controllers/EventoController.java
+++ src/main/java/com/i9/eventoapp/controllers/EventoController.java
@@ -1,7 +1,8 @@
-package com.i9.eventoapp.controllers; 
+package com.i9.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,29 +12,41 @@ import com.i9.eventoapp.repository.IEventoRepository;
 
 @Controller
 public class EventoController {
-	
+
 	@Autowired
 	private IEventoRepository eventoRepository;
-	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
 	}
-	
-	
-	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
 	public String form(Evento evento) {
 		eventoRepository.save(evento);
-		
+
 		return "redirect:/cadastrarEvento";
 	}
-	
-	
+
 	@RequestMapping("/eventos")
 	public ModelAndView listaEventos() {
-		
+
 		ModelAndView mv = new ModelAndView("index");
 		Iterable<Evento> eventos = eventoRepository.findAll();
-		mv.addObject("eventos",eventos);
+		mv.addObject("eventos", eventos);
 		return mv;
 	}
+
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") Long codigo) {
+
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+
+		mv.addObject("evento", evento);
+
+		return mv;
+
+	}
+
 }
--- src/main/java/com/i9/eventoapp/repository/IEventoRepository.java
+++ src/main/java/com/i9/eventoapp/repository/IEventoRepository.java
@@ -6,5 +6,7 @@ import com.i9.eventoapp.models.Evento;
 
 public interface IEventoRepository extends CrudRepository<Evento, String> {
 	
-
+	
+	Evento findByCodigo(Long codigo);
+		
 }
