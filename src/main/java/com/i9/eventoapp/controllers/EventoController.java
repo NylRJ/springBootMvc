8a4609ebba2ee76f36be15221c2a0df2f9cda2c7
Moises <loveloungeoficial@gmail.com>
2020-04-01 04:47:36 -0300

    cadastro metodo insert

--- src/main/java/com/i9/eventoapp/controllers/EventoController.java
+++ src/main/java/com/i9/eventoapp/controllers/EventoController.java
@@ -1,13 +1,28 @@
 package com.i9.eventoapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.i9.eventoapp.models.Evento;
+import com.i9.eventoapp.repository.IEventoRepository;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private IEventoRepository eventoRepository;
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		eventoRepository.save(evento);
+		
+		return "redirect:/cadastrarEvento";
 	}
 }
---
+++ src/main/java/com/i9/eventoapp/repository/IEventoRepository.java
@@ -0,0 +1,10 @@
+package com.i9.eventoapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.i9.eventoapp.models.Evento;
+
+public interface IEventoRepository extends CrudRepository<Evento, String> {
+	
+
+}
